a75edf265034b73a48bf9dfb2474ead6b1fc6812
nrosamalave <nrosamalave@gmail.com>
2024-05-15 23:23:25 -0400

    help

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ locals {
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name              = aws_s3_bucket.b.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    #origin_access_control_id = aws_cloudfront_origin_access_control.default.id
     origin_id                = local.s3_origin_id
   }
 
