b7286ecef04d6416ff309c02c1dafc84e5e239db
nrosamalave <nrosamalave@gmail.com>
2024-05-15 21:52:28 -0400

    initial commit

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "apikey" {
+  description = "aws api key"
+  type        = string
+  sensitive   = true
+}
+
+variable "secretkey" {
+  description = "aws secret key"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+    description = "AWS region"
+    type        = string
+    validation {
+        condition       = contains(["us-east-1", "us-east-2"], var.region)
+        error_message   = "Invalid AWS region: us-east-1, us-east-2"
+    } 
+}
