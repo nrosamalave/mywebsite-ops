30eef75f5b8caebc9f2bea89fa9248a7d2084389
nrosamalave <nrosamalave@gmail.com>
2024-05-17 15:42:08 -0400

    added db instance

--- database.tf
+++ database.tf
@@ -54,3 +54,25 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_db_subnet_group" "main" {
+  name       = "main"
+  subnet_ids = [aws_subnet.subnet1.id]
+}
+
+resource "aws_db_instance" "main" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  name                 = "mydatabase"
+  manage_master_user_password = true
+  username             = "admin"
+  db_subnet_group_name = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  skip_final_snapshot  = true
+
+  tags = {
+    Name = "terraform-rds"
+  }
+}
