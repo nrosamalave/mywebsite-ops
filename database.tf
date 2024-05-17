2d89e793c6fe17365aa147ec6f5262a1202999e8
nrosamalave <nrosamalave@gmail.com>
2024-05-17 15:35:21 -0400

    new db config

--- database.tf
+++ database.tf
@@ -8,49 +8,24 @@ resource "aws_subnet" "subnet1" {
   availability_zone = "us-east-1a" # Change to your preferred AZ
 }
 
-data "aws_ip_ranges" "cloudfront" {
-  services = ["CLOUDFRONT"]
-}
+resource "aws_security_group" "rds_sg" {
+  vpc_id      = aws_vpc.main.id
+  name        = "rds_security_group"
+  description = "Allow access to RDS"
 
-resource "aws_security_group" "allow_cloudfront_and_my_ip" {
-  name        = "allow_cloudfront_and_my_ip"
-  description = "Security group to allow access from CloudFront and my IP only"
-
-  dynamic "ingress" {
-    for_each = data.aws_ip_ranges.cloudfront.cidr_blocks
-    content {
-      description = "Allow CloudFront IP range"
-      from_port   = 0
-      to_port     = 65535
-      protocol    = "tcp"
-      cidr_blocks = [ingress.value]
-    }
-  }
-
-  # Ingress rule to allow access from your own IP
   ingress {
-    description      = "Allow access from my IP"
-    from_port        = 0
-    to_port          = 65535
-    protocol         = "tcp"
-    cidr_blocks      = ["174.179.163.53/32"]  # Replace YOUR_IP_ADDRESS with your actual IP address
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Change to your preferred CIDR
   }
 
-  # Egress rule to allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "allow_cloudfront_and_my_ip"
-  }
-}
-
-output "security_group_id" {
-  value = aws_security_group.allow_cloudfront_and_my_ip.id
 }
 
 resource "aws_security_group" "web_sg" {
@@ -69,7 +44,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Use your IP range for security
+    cidr_blocks = ["174.179.163.53/32"] # Use your IP range for security
   }
 
   egress {
